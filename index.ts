cb4b8c485238736d4faa3893f6022e6b9f08fa96
Samuel <samuel.fry13@gmail.com>
2025-01-20 18:01:49 -0500

    chore: update cli programs

--- index.ts
+++ index.ts
@@ -1,35 +1,57 @@
 import { parseArgs } from "util";
 import { $, ShellError } from "bun";
+import { Command } from "@commander-js/extra-typings";
 
-const printUsage = () => {
-  console.log(`Usage: bun run index.ts --year=<year> <day>`);
-};
+const program = new Command();
 
-// Runs a file based on year and day
-const { values, positionals } = parseArgs({
-  args: Bun.argv,
-  options: {
-    watch: {
-      type: "boolean",
-    },
-    year: {
-      type: "string",
-    },
-  },
-  allowPositionals: true,
-});
+program
+  .command("day")
+  .description("Run a specific day's solution")
+  .argument("<day>", "Day to run")
+  .option(
+    "-y, --year <year>",
+    "Year to run",
+    new Date().getFullYear().toString()
+  )
+  .option("-w, --watch", "Watch for changes")
+  .action(async (day, { year, watch }) => {
+    await $`bun run ${watch ? "--watch" : ""} ${year}/${day}/solution.ts`;
+  });
 
-const [_bun, _filename, day] = positionals;
-const { year = "2024", watch } = values;
+program
+  .command("generate")
+  .description("Generate solution files for a new day")
+  .argument("<day>", "Day to generate")
+  .option(
+    "-y, --year <year>",
+    "Year to generate",
+    new Date().getFullYear().toString()
+  )
+  .option("-o, --overwrite", "Overwrite existing files")
+  .action(async (day, { year, overwrite }) => {
+    const path = `./${year}/${day}`;
 
-try {
-  const parsedYear = Number(year);
-  if (!parsedYear || isNaN(parsedYear) || !day) {
-    printUsage();
-    await $`exit 1`;
-  }
+    if (Bun.file(`${path}/solution.ts`).exists() && !overwrite) {
+      return console.error(`Solution files already exist for ${year}/${day}`);
+    }
 
-  await $`bun run ${watch ? "--watch" : ""} ${year}/${day}/solution.ts`;
-} catch (err: any) {
-  if ("exitCode" in err) console.error(`Failed with code ${err.exitCode}`);
-}
+    await $`mkdir -p ${path}`;
+    await $`touch ${path}/input.txt`;
+    await $`touch ${path}/test.txt`;
+    await $`touch ${path}/solution.ts`;
+
+    const solutionFile = Bun.file(`${path}/solution.ts`);
+    const writer = solutionFile.writer();
+
+    writer.write(`import { readInput } from "../../util";\n\n`);
+    writer.write(`const input = await getInput(__dirname, "test.txt");\n\n`);
+    writer.write(`// ------------- PART 1 ---------------------\n\n`);
+    writer.write(`// ------------- PART 2 ---------------------\n\n`);
+    writer.end();
+
+    await $`code -r ${path}/input.txt`;
+    await $`code -r ${path}/test.txt`;
+    await $`code -r ${path}/solution.ts`;
+  });
+
+program.parse();
